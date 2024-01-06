4a8d5eb0eb02833492c002b5230219b787a2051f
Prajaktasangolkar <109715943+Prajaktasangolkar@users.noreply.github.com>
2024-01-06 14:29:09 +0530

    4

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,38 +1,30 @@
 
-import { useState } from 'react';
 import './App.css'
+import { useState } from 'react'
 
 function App() {
   let [counter,setCounter]=useState(15)
-  // let counter=5
   const addValue=()=>{
-    console.log("value added",Math.random());
-    if (counter<20){
-      setCounter(counter+1)
-    }
+    console.log('clicked',counter);
+    // counter=counter+1
+    setCounter(counter+1)
+  }
 
-    console.log(counter);
-    
-  }
   const removeValue=()=>{
-    console.log("value added",Math.random());
-    if (counter>0){
-      setCounter(counter-1)
-    }
+    console.log('clicked',counter);
+    // counter=counter-1
+    setCounter(counter-1)
   }
+
   return (
     <>
-     <h1>Chai aur react</h1>
-     <h2>Counter value:{counter}</h2>
-     <button
-     onClick={addValue}
-     >Add value</button>
-     <br />
-     <button
-     onClick={removeValue}>Remove value</button>
+      <h1>Chai aur react</h1>
+      <h2>Counter value:{counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br /><br />
+      <button onClick={removeValue}>Remove value </button>
     </>
   )
 }
-
-
 export default App
