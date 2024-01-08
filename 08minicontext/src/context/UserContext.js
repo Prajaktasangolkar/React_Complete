3e3c5fe6a22b1dc28cb4187ba69b6626ecf79561
Prajaktasangolkar <109715943+Prajaktasangolkar@users.noreply.github.com>
2024-01-08 22:25:54 +0530

    13

---
+++ 08minicontext/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+//Context is nothing but global provider inside that whatever will be done that will goes for all 
+// it will take all state excess
+const UserContext=React.createContext()
+
+export default UserContext;
