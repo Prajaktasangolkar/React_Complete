92a7d2a067da9dd3f4aeb53cc305b28660a3a980
Prajaktasangolkar <109715943+Prajaktasangolkar@users.noreply.github.com>
2024-01-11 18:51:52 +0530

    21_appwrite_config

---
+++ 12_MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,92 @@
+import conf from "../../conf/conf";
+
+// we want services so that connect with appwrite
+import { Client, Account, ID } from "appwrite";
+
+// inside class this will done when new object will be created
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    //if user acc will be there then do successfull login directly
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        //call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+   
+//   if i landed and home page check it is login or not
+async getCurrentUser(){
+    try{
+          return await this.account.get()
+    }
+    catch(error){
+           console.log("Appwrite service:: getCurrentUser::error",error);
+    }
+    return null
+}
+
+async logout(){
+    try {
+        await this.account.deleteSession()
+    } catch (error) {
+        console.log("Appwrite service::logout::error",error);
+    }
+}
+  
+}
+const authService = new AuthService();
+
+export default authService;
+
+
+
+/*
+import { Client, Account, ID } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('<PROJECT_ID>');               // Your project ID
+
+const account = new Account(client);
+
+const promise = account.create('[USER_ID]', 'email@example.com', '');
+
+promise.then(function (response) {
+    console.log(response); // Success
+}, function (error) {
+    console.log(error); // Failure
+});
+
+*/
---
+++ 12_MegaBlog/src/appwrite/config.js
@@ -0,0 +1,149 @@
+import conf from "../../conf/conf";
+
+// we want services so that connect with appwrite
+import { Client, ID ,Databases,Storage,Query} from "appwrite";
+
+export class Service{
+     client=new Client()
+     databases;
+     bucket;
+     constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+         this.databases=new Databases(this.client)
+         this.bucket=new Storage(this.client)
+     }
+
+     async createPost({title,slug,content,featuredImage,status,userId}){
+              try {
+                  return await this.databases.createDocument(
+                    conf.appwriteDatabaseId,
+                    conf.appwriteCollectionId,
+                    slug,
+                    {
+                        title,
+                        content,
+                        featuredImage,
+                        status,
+                        userId,
+                    }
+                  )
+              } catch (error) {
+                console.log("Appwrite service:: createPost::error",error);
+  
+              }
+     }
+
+     async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,          
+                }
+            )    
+        } catch (error) {
+            console.log("Appwrite service:: updatePost::error",error); 
+        }
+     }
+
+     async deletePost(slug){ //////////////////////////////////slug==id
+        try {
+             await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )    
+            return true;
+        } catch (error) {
+            console.log("Appwrite service:: updatePost::error",error); 
+            return false;
+        }
+     }
+
+     //this will give single post 
+     
+     async getPost(slug){ //////////////////////////////////slug==id
+        try {
+             await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )    
+            return true;
+        } catch (error) {
+            console.log("Appwrite service:: getPost::error",error); 
+            return false;
+        }
+
+      
+     }
+
+     //what to do for all the posts
+     //we will get all the post using listdocument but it also give all not active document
+     //that non active documents will be there 
+     //use query
+     async getPosts(queries=[Query.equal("status","active")]){//status is a indexes column
+        try {
+            await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+                // [
+                //     Query.equal("status","active")
+                // ]
+            ) 
+            return true;    
+        } catch (error) {
+            console.log("Appwrite service:: getPosts::error",error); 
+            return false
+        }
+    }
+
+    //file upload service
+    // storage services is this
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service:: uploadFile::error",error); 
+            return false;
+        }
+    }
+
+    async deletFile(fileId){
+                try {
+                    await this.bucket.deleteFile(
+                        conf.appwriteBucketId,
+                        fileId
+                    )
+                    return true;
+                } catch (error) {
+                    console.log("Appwrite service:: deleteFile::error",error); 
+                    return false;
+                }
+    }
+
+    getFilePrviwe(fileId){  //this is very fast so not need of async
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+
+
+const service=new Service()
+
+export default service
