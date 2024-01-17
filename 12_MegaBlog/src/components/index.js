32b8f63a2e4818b2fce3ba0b88d131ddb4102bf6
Prajaktasangolkar <109715943+Prajaktasangolkar@users.noreply.github.com>
2024-01-17 14:16:22 +0530

    26_buildingpages

--- 12_MegaBlog/src/components/index.js
+++ 12_MegaBlog/src/components/index.js
@@ -6,7 +6,16 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./input";
 import RTE from "./RTE";
-
+import Signup from "../Pages/SignUp";
+import Login from "../Pages/Login";
+import Post from "../Pages/Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout'
+import AddPost from '../Pages/AddPost'
+import AllPost from '../Pages/AllPost'
+import EditPost from "../Pages/EditPost";
+import Select from './Select'
 export {
     Header,
     Footer,
@@ -15,5 +24,15 @@ export {
     LogoutBtn,
     Button,
     Input,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    AddPost,
+    AllPost,
+    EditPost,  
+    Select
 }
